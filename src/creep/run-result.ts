137d4a63b198568c18ff6e10fd645719e690eac7
Alexey Krasnyanskiy <lex29071991@gmail.com>
2016-12-27 01:07:12 +0700

    Refactored role -> taskSequence

--- src/creep/run-result.ts
+++
@@ -1,4 +0,0 @@
-export enum RunResult {
-	DONE,
-	IN_PROGRESS
-}
--- src/creep/task/task.ts
+++ src/creep/task/task.ts
@@ -1,12 +1,45 @@
-import {RunResult} from '../run-result';
+export type TaskIn = {
+	creep: Creep,
+	data: any
+};
+
+
+export type TaskOut = {
+	status: TaskStatus,
+	data: any
+};
+
+export type TaskParams = {
+	inResolver: (...args: Array<any>) => any,
+	outResolver: (taskOut: TaskOut) => void
+};
+
+export enum TaskStatus {
+	DONE,
+	IN_PROGRESS
+}
 
 
 export interface Task {
 	name: string;
-	run(creep: RunParams): RunResult;
+	run(creep: TaskIn): TaskOut;
 }
 
-type RunParams = {
-	creep: Creep,
-	TaskData: any
+
+export abstract class TaskSequence implements Task {
+	public name: string;
+	protected _taskMap: Map<string, TaskParams>;
+	protected _taskStack: Array<string>;
+
+	constructor() {
+		this._taskMap = new Map();
+		this._taskStack = [];
+	}
+
+
+	public run(params: TaskIn): TaskOut {
+		throw params;
+	}
+
+
 }
